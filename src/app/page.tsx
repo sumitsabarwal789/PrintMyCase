d2ec89c1d4edd0cd6fcfaca663617c2dac831b82
Sumit Sabarwal <sumitsabarwal5@gmail.com>
2024-06-19 17:46:09 +0530

    Added order section and footer section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import { Icons } from "@/components/Icons";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
 import { Reviews } from "@/components/Reviews";
-import { Check, Star } from "lucide-react";
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowRight, Check, Star } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -37,15 +39,15 @@ flex-col items-center sm:items-start"
               >
                 <div className="space-y-2">
                   <li className="flex gap-1.5 items-center text-left">
-                    <Check className="h-5 w-5 shrink-0 ☐ text-purple-600 " />
+                    <Check className="h-5 w-5 shrink-0 ☐ text-purple-700 " />
                     High-quality, durable material
                   </li>
                   <li className="flex gap-1.5 items-center text-left">
-                    <Check className="h-5 w-5 shrink-0 ☐ text-purple-600 " />3
+                    <Check className="h-5 w-5 shrink-0 ☐ text-purple-700 " />3
                     year print guarantee
                   </li>
                   <li className="flex gap-1.5 items-center text-left">
-                    <Check className="h-5 w-5 shrink-0 ☐ text-purple-600 " />
+                    <Check className="h-5 w-5 shrink-0 ☐ text-purple-700 " />
                     All iphone models supported
                   </li>
                 </div>
@@ -90,23 +92,23 @@ ring-slate-100"
                 <div className="flex flex-col justify-between items-center sm:items-start">
                   <div className="flex gap-0.5 ">
                     <Star
-                      className="h-4 w-4 ☐ text-purple-600
+                      className="h-4 w-4 ☐ text-purple-700
 fill-purple-500"
                     />
                     <Star
-                      className="h-4 w-4 text-purple-600
+                      className="h-4 w-4 text-purple-700
 fill-purple-500"
                     />
                     <Star
-                      className="h-4 w-4 ☐ text-purple-600
+                      className="h-4 w-4 ☐ text-purple-700
 fill-purple-500"
                     />
                     <Star
-                      className="h-4 w-4 ☐ text-purple-600
+                      className="h-4 w-4 ☐ text-purple-700
 fill-purple-500"
                     />
                     <Star
-                      className="h-4 w-4 ☐ text-purple-600
+                      className="h-4 w-4 ☐ text-purple-700
 fill-purple-500"
                     />
                   </div>
@@ -158,7 +160,7 @@ text-gray-900"
               What our{" "}
               <span className="relative px-2">
                 customers{" "}
-                <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-purple-600" />
+                <Icons.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-purple-700" />
               </span>{" "}
               say
             </h2>
@@ -176,11 +178,11 @@ text-gray-900"
           >
             <div className="flex flex-auto flex-col gap-4 lg:pr-8 x1:pr-20">
               <div className="flex gap-0.5 mb-2">
-                <Star className="h-5 w-5  text-purple-600 ☐ fill-purple-500" />
-                <Star className="h-5 w-5  text-purple-600 ☐ fill-purple-500" />
-                <Star className="h-5 w-5  text-purple-600 ☐ fill-purple-500" />
-                <Star className="h-5 w-5  text-purple-600 ☐ fill-purple-500" />
-                <Star className="h-5 w-5 text-purple-600 ☐ fill-purple-500" />
+                <Star className="h-5 w-5  text-purple-700 ☐ fill-purple-500" />
+                <Star className="h-5 w-5  text-purple-700 ☐ fill-purple-500" />
+                <Star className="h-5 w-5  text-purple-700 ☐ fill-purple-500" />
+                <Star className="h-5 w-5  text-purple-700 ☐ fill-purple-500" />
+                <Star className="h-5 w-5 text-purple-700 ☐ fill-purple-500" />
               </div>
               <div className="text-lg leading-8">
                 <p>
@@ -213,11 +215,11 @@ text-gray-900"
             {/* customer 2 */}
             <div className="flex flex-auto flex-col gap-4 lg:pr-8 x1:pr-20">
               <div className="flex gap-0.5 mb-2">
-                <Star className="h-5 w-5  text-purple-600 ☐ fill-purple-500" />
-                <Star className="h-5 w-5  text-purple-600 ☐ fill-purple-500" />
-                <Star className="h-5 w-5  text-purple-600 ☐ fill-purple-500" />
-                <Star className="h-5 w-5  text-purple-600 ☐ fill-purple-500" />
-                <Star className="h-5 w-5 text-purple-600 ☐ fill-purple-500" />
+                <Star className="h-5 w-5  text-purple-700 ☐ fill-purple-500" />
+                <Star className="h-5 w-5  text-purple-700 ☐ fill-purple-500" />
+                <Star className="h-5 w-5  text-purple-700 ☐ fill-purple-500" />
+                <Star className="h-5 w-5  text-purple-700 ☐ fill-purple-500" />
+                <Star className="h-5 w-5 text-purple-700 ☐ fill-purple-500" />
               </div>
               <div className="text-lg leading-8">
                 <p>
@@ -253,6 +255,72 @@ text-gray-900"
         <div className="pt-16">
           <Reviews />
         </div>
+      </section>
+
+      {/* buying section */}
+      <section className="py-4">
+        <MaxWidthWrapper className="py-24">
+          <div className="mb-12 px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl sm:text-center">
+              <h2 className="order-1  tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900 my-2">
+                Upload your photo and get{" "}
+                <span className="relative px-2 bg-purple-600 text-white rounded-sm py-1">
+                  your own case
+                </span>{" "}
+                now
+              </h2>
+            </div>
+          </div>
+
+          <div className="mx-auto max-w-6xl px-6 lg:px-8 pt-10">
+            <div className="relative flex flex-col items-center md:grid grid-cols-2 gap-40">
+              <img
+                src="/arrow.png"
+                className="absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0"
+              />
+
+              <div className="relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl">
+                <img
+                  src="/horse.jpg"
+                  className="rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full"
+                />
+              </div>
+
+              <Phone className="w-60" imgSrc="/horse_phone.jpg" />
+            </div>
+          </div>
+
+          <ul className="mx-auto mt-12 max-w-prose sm:text-lg space-y-2 w-fit">
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-purple-700 inline mr-1.5" />
+              High-quality silicone material
+            </li>
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-purple-700 inline mr-1.5" />
+              Scratch- and fingerprint resistant coating
+            </li>
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-purple-700 inline mr-1.5" />
+              Wireless charging compatible
+            </li>
+            <li className="w-fit">
+              <Check className="h-5 w-5 text-purple-700 inline mr-1.5" />
+              High quality Printing
+            </li>
+
+            <div className="flex justify-center">
+              <Link
+                className={buttonVariants({
+                  size: "lg",
+                  className: "mx-auto mt-8",
+                })}
+                href="/configure/upload"
+              >
+                Create your case now <ArrowRight className="h-4 w-4 ml-1.5" />
+              </Link>
+            </div>
+          </ul>
+        </MaxWidthWrapper>
       </section>
     </main>
   );
