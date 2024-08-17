fc5e7c5fb2dfbcd9e853904824a9e6d2acba6d20
Sumit Sabarwal <sumitsabarwal5@gmail.com>
2024-08-17 22:53:37 +0530

    Updated a logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
             <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
               <div className="absolute w-28 left-0 -top-20 hidden lg:block">
-                <img src="/logo-2.png" className="w-full" />
+                <img src="/logo-1.png" className="w-[40rem]" />
               </div>
               <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
                 Your image on a{" "}
